9de32ba724e27e37434490adb330d21bd7bfe78a
unknown <roco.jomari29@gmail.com>
2026-01-28 18:22:19 +0800

    added about us page, coffee menu page, fixing bugs in the home page

---
+++ src/api/coffee-menu-page/routes/coffee-menu-page.ts
@@ -0,0 +1,7 @@
+/**
+ * coffee-menu-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::coffee-menu-page.coffee-menu-page');
---
+++ src/api/coffee-menu-page/services/coffee-menu-page.ts
@@ -0,0 +1,7 @@
+/**
+ * coffee-menu-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::coffee-menu-page.coffee-menu-page');
